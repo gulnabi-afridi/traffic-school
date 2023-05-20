50f45efd85526b6193004717afff119fa170310d
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-05-20 15:49:59 +0500

    again server side issues reviwed

--- src/views/Home/WeAlsoOffer.tsx
+++ src/views/Home/WeAlsoOffer.tsx
@@ -15,8 +15,8 @@ function WeAlsoOffer() {
         <div className="w-full grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-6 sm:py-10">
           {Data.CoursesWeOffer.map((item, index) => {
             return (
-              <div className="w-full flex justify-center items-center">
-                <WeAlsoOfferCard key={index} image={item.img} name={item.name} />
+              <div key={index} className="w-full flex justify-center items-center">
+                <WeAlsoOfferCard image={item.img} name={item.name} />
               </div>
             );
           })}
